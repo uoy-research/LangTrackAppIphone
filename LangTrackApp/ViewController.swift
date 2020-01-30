7b5170b458aede98718746ab9445aa0a01814b63
stephanbjorck <stephan.bjorck@humlab.lu.se>
2020-01-30 10:46:32 +0100

    Initial Commit

---
+++ LangTrackApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LangTrackApp
+//
+//  Created by Stephan Björck on 2020-01-30.
+//  Copyright © 2020 Stephan Björck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
