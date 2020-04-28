04ec569d029df7b2ea488ef8a566e5a3c95660bd
stephanbjorck <stephan.bjorck@humlab.lu.se>
2020-04-28 09:30:51 +0200

    Toast viewController extension

--- LangTrackApp/Extentions/ViewExtension.swift
+++ LangTrackApp/Extentions/ViewExtension.swift
@@ -26,3 +26,33 @@ extension UIView{
         self.layer.shadowOpacity = 1
     }
 }
+
+extension UIViewController {
+    
+    func showToast(message : String, font: UIFont) {
+        let padding: CGFloat = 15
+        var textWidth = message.width(withConstrainedHeight: 35, font: font) + padding
+        if textWidth > (view.frame.size.width - padding){
+            textWidth = view.frame.size.width - padding
+        }
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - (textWidth / 2), y: self.view.frame.size.height-80, width: textWidth, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.65)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 0.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 0.6, delay: 0.0, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 1.0
+        }, completion: {(isCompleted) in
+            UIView.animate(withDuration: 1.0, delay: 2.2, options: .curveEaseOut, animations: {
+                toastLabel.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastLabel.removeFromSuperview()
+            })
+        })
+    }
+}
