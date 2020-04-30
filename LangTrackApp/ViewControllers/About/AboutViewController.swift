050096d3175f0539ecb08b8580d3f464cf29c69e
stephanbjorck <stephan.bjorck@humlab.lu.se>
2020-04-30 22:17:26 +0200

    AboutViewController and ContactViewController

---
+++ LangTrackApp/ViewControllers/About/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  LangTrackApp
+//
+//  Created by Stephan Björck on 2020-04-30.
+//  Copyright © 2020 Stephan Björck. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closeButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ LangTrackApp/ViewControllers/Contact/ContactViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ContactViewController.swift
+//  LangTrackApp
+//
+//  Created by Stephan Björck on 2020-04-30.
+//  Copyright © 2020 Stephan Björck. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
